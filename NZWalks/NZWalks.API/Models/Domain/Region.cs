f70e84c530f46fe624c3c04ad13a56e3d01696fb
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2022-09-10 14:42:50 +0200

    Added models and installed EF packages

---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,14 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Latitue { get; set; }
+        public double Longtitude { get; set; }
+        public long Population { get; set; }
+    }
+}
+
---
+++ NZWalks/NZWalks.API/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class WalkDifficultyId
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }
+}
